0c88129dae81434c0b38075c9d20a03a20429c6a
SomilBansalQA <somilbansal@qainfotech.com>
2017-07-27 17:10:32 +0530

    Update TatocAdvance.java

--- src/test/java/org/qait/tatoc/Action/tatoc1/TatocAdvance.java
+++ src/test/java/org/qait/tatoc/Action/tatoc1/TatocAdvance.java
@@ -1,30 +1,31 @@
 package org.qait.tatoc.Action.tatoc1;
 
+import static com.jayway.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+import com.jayway.restassured.http.ContentType;
+
 public class TatocAdvance {
 
 	WebDriver driver;
 	Tatocdatabase tatocdatabaseobj;
-	PropertiesFileReader dataObj;
+
 	By advanceCourse = By.linkText("Advanced Course");
 	By menu2 = By.className("m2");
 	By goNext = By.cssSelector(".menuitem:nth-child(5)");
-
-	
 	By symbolDisplay= By.id("symboldisplay");
 	By name=By.id("name");
 	By passkey=By.id("passkey");
 	By proceed=By.id("submit");
-	
-	
+
 	public TatocAdvance(WebDriver driver) {
 		this.driver = driver;
 		tatocdatabaseobj =new Tatocdatabase();
-		dataObj = new PropertiesFileReader("database.properties");
 	}
 
 	public void clickOnAdvanceCourse() {
@@ -37,21 +38,21 @@ public class TatocAdvance {
 		Actions action = new Actions(driver);
 		action.moveToElement(element).build().perform();
 		driver.findElement(goNext).click();
-
 	}
 
-	
 	public void clickOnProceed(){
 		driver.findElement(proceed).click();
 	}
 	
 	public void submitValidcredentials(){
-	
 		String Symbol=driver.findElement(symbolDisplay).getText();
-		tatocdatabaseobj.FetchUserNameAndPassWordAndThenSetinDatabasePropertyFile(Symbol);
-		driver.findElement(name).sendKeys((String)dataObj.getOptionValue("username"));
-		driver.findElement(passkey).sendKeys((String)dataObj.getOptionValue("password"));
+		String userNameAndPassword=tatocdatabaseobj.getUserNameAndPasswordFromCredential(Symbol);
+		System.out.println(userNameAndPassword);
+		String[] words=userNameAndPassword.split("\\s");
+		driver.findElement(name).sendKeys(words[0]);
+		driver.findElement(passkey).sendKeys(words[1]);
 		clickOnProceed();
 	}
+	
 	
 }
